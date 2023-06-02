0071ffc2209c91ba5e0dbe027ee94e98ba4f2dad
feven-t <114930667+feven-t@users.noreply.github.com>
2023-06-02 20:17:09 +0300

    UI

---
+++ chefs_table/lib/NavBar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.all(0),
+        children: [
+          InkWell(
+            onTap: () => context.push('/profile'),
+            child: UserAccountsDrawerHeader(
+                accountName: TextButton(
+                  child: Text('Abebe'),
+                  onPressed: () {},
+                ),
+                accountEmail: const Text('Abebe@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                    // child: ClipOval(
+                    //     child: Image.asset('food.png',
+                    //         width: 80, height: 80, fit: BoxFit.cover)),
+                    ),
+                decoration: const BoxDecoration(color: Colors.brown)),
+          ),
+          ListTile(
+            leading: const Icon(Icons.house),
+            title: const Text('Home'),
+            onTap: () {
+              context.go('/home');
+            },
+          ),
+          ListTile(
+            //leading: const Icon(Icons.house),
+            title: const Text('Restaurant'),
+            onTap: () {
+              context.go('/restaurant');
+            },
+          ),
+          ListTile(
+            //leading: const Icon(Icons.house),
+            title: const Text('Recipies'),
+            onTap: () {
+              context.go('/recipe');
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
